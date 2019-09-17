9d5323b4c6c57c0e71e6ec627bdd5205973e8266
pakoig10 <jiglesias1@ucol.mx>
2019-09-17 16:14:50 -0500

    Show the lists and tasks only to the creator(user)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,33 @@
 class TasksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.create(tasks_params)
+    redirect_to list_path(@list)
+  end
 
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    if @task.(comment_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def tasks_params
+      params.require(:task).permit(:title, :content)
+    end
 end
